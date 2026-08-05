570b4d3bb6be4fdc668c9380570876a02f34bc58
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-04 19:32:18 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE="snp-planner-v4";
+const FILES=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html")))));
